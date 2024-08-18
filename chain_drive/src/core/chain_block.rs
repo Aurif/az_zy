e546475f904359973330e8075b67aa933695c6a0
Filmos <filmosyt@gmail.com>
2024-08-18 23:11:04 +0200

    C-ChainBlock - deduplicated define_block macro

--- chain_drive/src/core/chain_block.rs
+++ chain_drive/src/core/chain_block.rs
@@ -37,54 +37,21 @@ impl<R: ChainBlockRef> ChainBlockInserter for Arc<Mutex<R>> {
 #[macro_export]
 macro_rules! define_block {
     ( $vis:vis struct $name:ident; $(impl { $($impl_code:tt)* })? $(impl for $pos:ident, $t:ty { $($code:tt)* })* ) => {
-        mod __inner {
-            pub use std::sync::{Arc, Mutex};
-            pub use chain_drive::{ChainDrive};
-            pub use chain_drive::in_macro::{ChainBlock, ChainBlockRef};
-            macro_rules! chain_block_insert {
-                (ChainBFront, $ti:ty, $rf:ident, $chain_drive:ident) => {
-                    $chain_drive.push_front($rf.clone() as __inner::Arc<__inner::Mutex<dyn __inner::ChainBlock<$ti, ChainBFront>>>);
-                };
-                (ChainBBack, $ti:ty, $rf:ident, $chain_drive:ident) => {
-                    $chain_drive.push_back($rf.clone() as __inner::Arc<__inner::Mutex<dyn __inner::ChainBlock<$ti, ChainBBack>>>);
-                };
-            }
-            pub(crate) use chain_block_insert;
-        }
         $vis struct $name;
         $(
             impl $name {
                 $($impl_code)*
             }
         )?
-        $(
-            impl __inner::ChainBlock<$t, $pos> for $name {
-                $($code)*
-            }
-        )*
-        impl __inner::ChainBlockRef for $name {
-            fn insert_into(self_ref: __inner::Arc<__inner::Mutex<Self>>, chain_drive: &mut __inner::ChainDrive) {
-                $(
-                    __inner::chain_block_insert!($pos, $t, self_ref, chain_drive);
-                )*
-            }
-        }
+        define_block!(inner $name
+            $(
+                impl for $pos, $t {
+                    $($code)*
+                }
+            )*
+        );
     };
     ( $vis:vis struct $name:ident $({ $($struct_code:tt)* })? $(impl { $($impl_code:tt)* })? $(impl for $pos:ident, $t:ty { $($code:tt)* })* ) => {
-        mod __inner {
-            pub use std::sync::{Arc, Mutex};
-            pub use chain_drive::{ChainDrive};
-            pub use chain_drive::in_macro::{ChainBlock, ChainBlockRef};
-            macro_rules! chain_block_insert {
-                (ChainBFront, $ti:ty, $rf:ident, $chain_drive:ident) => {
-                    $chain_drive.push_front($rf.clone() as __inner::Arc<__inner::Mutex<dyn __inner::ChainBlock<$ti, ChainBFront>>>);
-                };
-                (ChainBBack, $ti:ty, $rf:ident, $chain_drive:ident) => {
-                    $chain_drive.push_back($rf.clone() as __inner::Arc<__inner::Mutex<dyn __inner::ChainBlock<$ti, ChainBBack>>>);
-                };
-            }
-            pub(crate) use chain_block_insert;
-        }
         $vis struct $name $(
             {
                 $($struct_code)*
@@ -95,6 +62,29 @@ macro_rules! define_block {
                 $($impl_code)*
             }
         )?
+        define_block!(inner $name
+            $(
+                impl for $pos, $t {
+                    $($code)*
+                }
+            )*
+        );
+    };
+    ( inner $name:ident $(impl for $pos:ident, $t:ty { $($code:tt)* })* ) => {
+        mod __inner {
+            pub use std::sync::{Arc, Mutex};
+            pub use chain_drive::{ChainDrive};
+            pub use chain_drive::in_macro::{ChainBlock, ChainBlockRef};
+            macro_rules! chain_block_insert {
+                (ChainBFront, $ti:ty, $rf:ident, $chain_drive:ident) => {
+                    $chain_drive.push_front($rf.clone() as __inner::Arc<__inner::Mutex<dyn __inner::ChainBlock<$ti, ChainBFront>>>);
+                };
+                (ChainBBack, $ti:ty, $rf:ident, $chain_drive:ident) => {
+                    $chain_drive.push_back($rf.clone() as __inner::Arc<__inner::Mutex<dyn __inner::ChainBlock<$ti, ChainBBack>>>);
+                };
+            }
+            pub(crate) use chain_block_insert;
+        }
         $(
             impl __inner::ChainBlock<$t, $pos> for $name {
                 $($code)*
